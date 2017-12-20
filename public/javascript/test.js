e2e0a352790c8b5863a8c64db3d2730c97e54d0e
Yu-Shan Sun <yushans@g.clemson.edu>
2017-12-20 12:20:09 -0500

    Last?

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -7,11 +7,12 @@ function addNewCodeMirrorGutter(cmEditor) {
     var i;
     for (i = 0; i < vcs.length; i++) {
         // Check to see if there is already a vc marker at that line
+        var vcMarker;
         if (cmEditor.lineInfo(Number(vcs[i].line)).gutterMarkers !== undefined) {
-            var vcMarker = cmEditor.lineInfo(Number(vcs[i].line)).gutterMarkers.vcs;
+            vcMarker = cmEditor.lineInfo(Number(vcs[i].line)).gutterMarkers.vcs;
             vcMarker.setAttribute("title", vcMarker.getAttribute("title") + ", " + vcs[i].vcID);
         } else {
-            var vcMarker = document.createElement("span");
+            vcMarker = document.createElement("span");
             vcMarker.setAttribute("class", "badge badge-pill badge-primary");
             vcMarker.setAttribute("data-toggle", "tooltip");
             vcMarker.setAttribute("data-placement", "left");
@@ -29,11 +30,11 @@ function addNewCodeMirrorGutter(cmEditor) {
  */
 function createProverResults() {
     var vcResultArray = [];
-    vcResultArray.push(new fakeProveMessage(new fakeProveResult("0_1", "Proved, 0 ms")));
-    vcResultArray.push(new fakeProveMessage(new fakeProveResult("0_2", "Timeout after 3000 ms")));
-    vcResultArray.push(new fakeProveMessage(new fakeProveResult("0_3", "Proved, 24 ms")));
-    vcResultArray.push(new fakeProveMessage(new fakeProveResult("1_1", "Timeout after 3000 ms")));
-    vcResultArray.push(new fakeProveMessage(new fakeProveResult("1_2", "Proved, 1 ms")));
+    vcResultArray.push(new FakeProveMessage(new FakeProveResult("0_1", "Proved, 0 ms")));
+    vcResultArray.push(new FakeProveMessage(new FakeProveResult("0_2", "Timeout after 3000 ms")));
+    vcResultArray.push(new FakeProveMessage(new FakeProveResult("0_3", "Proved, 24 ms")));
+    vcResultArray.push(new FakeProveMessage(new FakeProveResult("1_1", "Timeout after 3000 ms")));
+    vcResultArray.push(new FakeProveMessage(new FakeProveResult("1_2", "Proved, 1 ms")));
 
     return vcResultArray;
 }
@@ -58,7 +59,7 @@ function decode(content) {
 /*
  * This creates a fake prover message object.
  */
-function fakeProveMessage(resultData) {
+function FakeProveMessage(resultData) {
     this.job = "verify";
     this.status = "complete";
     this.result = resultData;
@@ -67,7 +68,7 @@ function fakeProveMessage(resultData) {
 /*
  * This creates a fake prover result object.
  */
-function fakeProveResult(id, resultData) {
+function FakeProveResult(id, resultData) {
     this.id = id;
     this.result = resultData;
 }
@@ -75,7 +76,7 @@ function fakeProveResult(id, resultData) {
 /*
  * This creates a fake VC message object.
  */
-function fakeVCMessage() {
+function FakeVCMessage() {
     this.job = "genVCs";
     this.status = "complete";
     this.result = "%3CvcFile%3E%7B%22vcs%22%3A%5B%7B%22vcGivens%22%3A%221%253A%252520%25281%252520%253C%253D%252520%257CQ%2527%2527%257C%2529%250A%22%2C%22sourceFile%22%3A%22Iterative_Append_Realiz.rb%22%2C%22vcGoal%22%3A%22%25281%252520%253C%253D%252520%257CQ%2527%2527%257C%2529%250A%22%2C%22lineNum%22%3A%2211%22%2C%22vc%22%3A%220_1%22%2C%22vcInfo%22%3A%22Requires%252520Clause%252520of%252520Dequeue%252520in%252520Procedure%252520Append%253A%252520Iterative_Append_Realiz.rb%252811%2529%22%7D%2C%7B%22vcGivens%22%3A%221%253A%252520%2528%257CQ%2527%257C%252520%253C%253D%252520Max_Length%2529%250A2%253A%252520%2528Q%2527%2527%252520%253D%252520%2528%253CE%2527%253E%252520o%252520Q%2527%2529%2529%250A3%253A%252520%25281%252520%253C%253D%252520%257CQ%2527%2527%257C%2529%250A4%253A%252520%2528%2528%257CP%257C%252520%252B%252520%257CQ%257C%2529%252520%253C%253D%252520Max_Length%2529%250A5%253A%252520%2528%257CP%257C%252520%253C%253D%252520Max_Length%2529%250A6%253A%252520%2528%257CQ%257C%252520%253C%253D%252520Max_Length%2529%250A7%253A%252520%2528min_int%252520%253C%253D%252520Max_Length%2529%250A8%253A%252520%2528Max_Length%252520%253C%253D%252520max_int%2529%250A9%253A%252520%2528min_int%252520%253C%253D%2525200%2529%250A10%253A%252520%25281%252520%253C%253D%252520max_int%2529%250A11%253A%252520%25281%252520%253C%253D%252520Max_Length%2529%250A%22%2C%22sourceFile%22%3A%22Iterative_Append_Realiz.rb%22%2C%22vcGoal%22%3A%22%2528%25281%252520%252B%252520%257CP%2527%257C%2529%252520%253C%253D%252520Max_Length%2529%250A%22%2C%22lineNum%22%3A%2212%22%2C%22vc%22%3A%220_2%22%2C%22vcInfo%22%3A%22Requires%252520Clause%252520of%252520Enqueue%252520in%252520Procedure%252520Append%253A%252520Iterative_Append_Realiz.rb%252812%2529%22%7D%2C%7B%22vcGivens%22%3A%221%253A%252520%2528Q%2527%2527%252520%253D%252520%2528%253CE%2527%253E%252520o%252520Q%2527%2529%2529%250A2%253A%252520%2528%257CQ%2527%257C%252520%253C%253D%252520Max_Length%2529%250A3%253A%252520%25281%252520%253C%253D%252520%257CQ%2527%2527%257C%2529%250A4%253A%252520%2528%2528%257CP%257C%252520%252B%252520%257CQ%257C%2529%252520%253C%253D%252520Max_Length%2529%250A5%253A%252520%2528%257CP%257C%252520%253C%253D%252520Max_Length%2529%250A6%253A%252520%2528%257CQ%257C%252520%253C%253D%252520Max_Length%2529%250A7%253A%252520%2528min_int%252520%253C%253D%252520Max_Length%2529%250A8%253A%252520%2528Max_Length%252520%253C%253D%252520max_int%2529%250A9%253A%252520%2528min_int%252520%253C%253D%2525200%2529%250A10%253A%252520%25281%252520%253C%253D%252520max_int%2529%250A11%253A%252520%25281%252520%253C%253D%252520Max_Length%2529%250A%22%2C%22sourceFile%22%3A%22Iterative_Append_Realiz.rb%22%2C%22vcGoal%22%3A%22%2528%25281%252520%252B%252520%257CQ%2527%257C%2529%252520%253C%253D%252520%257CQ%2527%2527%257C%2529%250A%22%2C%22lineNum%22%3A%229%22%2C%22vc%22%3A%220_3%22%2C%22vcInfo%22%3A%22Termination%252520of%252520While%252520Statement%253A%252520Iterative_Append_Realiz.rb%25289%2529%22%7D%2C%7B%22vcGivens%22%3A%221%253A%252520not%2528%25281%252520%253C%253D%252520%257CQ%2527%257C%2529%2529%250A2%253A%252520%2528%2528%257CP%257C%252520%252B%252520%257CQ%257C%2529%252520%253C%253D%252520Max_Length%2529%250A3%253A%252520%2528%257CP%257C%252520%253C%253D%252520Max_Length%2529%250A4%253A%252520%2528%257CQ%257C%252520%253C%253D%252520Max_Length%2529%250A5%253A%252520%2528min_int%252520%253C%253D%252520Max_Length%2529%250A6%253A%252520%2528Max_Length%252520%253C%253D%252520max_int%2529%250A7%253A%252520%2528min_int%252520%253C%253D%2525200%2529%250A8%253A%252520%25281%252520%253C%253D%252520max_int%2529%250A9%253A%252520%25281%252520%253C%253D%252520Max_Length%2529%250A%22%2C%22sourceFile%22%3A%22Iterative_Append_Realiz.rb%22%2C%22vcGoal%22%3A%22%2528P%2527%252520%253D%252520%2528P%252520o%252520Q%2529%2529%250A%22%2C%22lineNum%22%3A%223%22%2C%22vc%22%3A%221_1%22%2C%22vcInfo%22%3A%22Ensures%252520Clause%252520of%252520Append%253A%252520Iterative_Append_Realiz.rb%25283%2529%22%7D%2C%7B%22vcGivens%22%3A%221%253A%252520not%2528%25281%252520%253C%253D%252520%257CQ%2527%257C%2529%2529%250A%22%2C%22sourceFile%22%3A%22Iterative_Append_Realiz.rb%22%2C%22vcGoal%22%3A%22%2528Q%2527%252520%253D%252520Empty_String%2529%250A%22%2C%22lineNum%22%3A%223%22%2C%22vc%22%3A%221_2%22%2C%22vcInfo%22%3A%22Ensures%252520Clause%252520of%252520Append%253A%252520Iterative_Append_Realiz.rb%25283%2529%22%7D%5D%7D%3C%2FvcFile%3E";
@@ -85,7 +86,7 @@ function fakeVCMessage() {
  * This function returns an array of VCs.
  */
 function getVCs() {
-    var vcResult = new fakeVCMessage();
+    var vcResult = new FakeVCMessage();
     var vcArray = [];
     var result = decode(vcResult.result);
     var savedVCs = $(result).text();
